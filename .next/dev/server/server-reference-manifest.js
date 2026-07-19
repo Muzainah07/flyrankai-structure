e988fd2550dab74891358e69408e128e9903b9e4
Muzainah07 <muzainahfaisal.mf@gmail.com>
2026-07-20 00:34:12 +0500

    chore: stop tracking .next build output

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kDjXSGolLRc+jpeBjx2ZWAsh+mKPQPD7o2gaZ2DVPlM=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__00t83_d._.css","static/chunks/node_modules_next_1ohmjn5._.js","static/chunks/src_components_Nav_tsx_0-8vgm_._.js"],
-    source: "dynamic"
-});
